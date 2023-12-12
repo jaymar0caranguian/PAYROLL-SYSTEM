6efcb243a086d0f5c01adc70f2aaa45ea1625e8c
Mark James Aropon <aropon.markjames.august17@gmail.com>
2023-12-12 12:52:47 +0800

    add necessary changes

--- WebApplication4/obj/Debug/net7.0/WebApplication4.AssemblyInfo.cs
+++ WebApplication4/obj/Debug/net7.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3347cc7bb8a662a502b6428cd2e80668698d668")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApplication4/wwwroot/js/site.js
+++ WebApplication4/wwwroot/js/site.js
@@ -3,3 +3,66 @@
 
 // Write your JavaScript code.
 
+//Generate Report
+function downloadPdf() {
+    const headingDiv = document.createElement('div');
+    headingDiv.innerHTML = '<h1>Attendance Report</h1>';
+    const tableElement = document.getElementById('table');
+    const containerDiv = document.createElement('div');
+    containerDiv.appendChild(headingDiv);
+    containerDiv.appendChild(tableElement);
+
+    html2pdf(containerDiv, {
+        margin: 10,
+        filename: 'Attendance Report.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    });
+}
+$(document).ready(function () {
+    $("#download").click(function () {
+        downloadPdf();
+    });
+});
+
+//Create new attendance
+$(document).ready(function () {
+    $('#createPartial').submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (data) {
+                if (data.success) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error!',
+                        text: 'Error creating attendance record.',
+                        confirmButtonText: 'OK'
+                    });
+                } else {             
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success!',
+                        text: 'Attendance record created successfully!',
+                        confirmButtonText: 'OK'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            location.reload();
+                        }
+                    });
+                }
+            },
+            error: function (xhr, status, error) {
+                alert('Error: ' + xhr.responseText); // Alert the error details
+            }
+        });
+    });
+
+    $('#saveButton').click(function () {
+        $('#createPartial').submit();
+    });
+});
