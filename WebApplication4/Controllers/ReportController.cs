c7df344a7e7f22c3c740b1d3cbce4be84fd3920b
trixiadu <trixiadu50@gmail.com>
2023-12-16 00:34:03 +0800

    attendance,reports(next and previous

--- WebApplication4/Controllers/ReportController.cs
+++ WebApplication4/Controllers/ReportController.cs
@@ -18,20 +18,33 @@ namespace WebApplication4.Controllers
             _context = context;
         }
         // GET: Report
-        public async Task<IActionResult> Index(DateTime? selectedDate)
+        public async Task<IActionResult> Index(DateTime? selectedDate, int page = 1, int pageSize = 10)
         {
             ViewBag.DateSortParam = "Date";
             var data = _context.Attds.AsQueryable();
+
+            // Apply date filter
             if (selectedDate.HasValue)
             {
                 data = data.Where(d => d.Date.Date == selectedDate.Value.Date);
             }
 
-            data = data.OrderBy(d => d.Date);
+            // Calculate the total number of records
+            int totalRecords = await data.CountAsync();
+
+            // Implement pagination
+            data = data.OrderBy(d => d.Date).Skip((page - 1) * pageSize).Take(pageSize);
+
             var model = await data.ToListAsync();
+
+            // Pass pagination information to the view
+            ViewBag.TotalRecords = totalRecords;
+            ViewBag.PageSize = pageSize;
+            ViewBag.CurrentPage = page;
 
             return View(model);
         }
+
         private bool AttdExists(int id)
         {
           return (_context.Attds?.Any(e => e.AttdId == id)).GetValueOrDefault();
