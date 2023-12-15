ca65039411acb8fabb2e64f632d74b66aa48ffb2
Jaymar Caranguian <145180662+jaymar0caranguian@users.noreply.github.com>
2023-12-16 04:57:53 +0800

    update nav bar, and delelete files

--- WebApplication4/DataDB/PmsDatabaseContext.cs
+++ WebApplication4/DataDB/PmsDatabaseContext.cs
@@ -31,7 +31,7 @@ public partial class PmsDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-B24CA6DU;Initial Catalog=PMS_DATABASE;Integrated Security=True; TrustServerCertificate = True");
+        => optionsBuilder.UseSqlServer("Data Source=QCU;Initial Catalog=PMS_DATABASE;Integrated Security=True; TrustServerCertificate = True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
